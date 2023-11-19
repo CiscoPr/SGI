465e023a11823b1e9653d59c3e50b752f52ddaf3
Francisco Prada <up202004646@up.pt>
2023-11-19 13:23:16 +0000

    added camera methods to the gui

--- tp2/MyGuiInterface.js
+++ tp2/MyGuiInterface.js
@@ -31,7 +31,19 @@ class MyGuiInterface  {
     init() {
         // adds a folder to the gui interface for the camera
         const cameraFolder = this.datgui.addFolder('Camera')
-        
+        const cameraNames = ["Perspective", "Right", "Left", "Top", "Bottom"]
+        console.log("my cameras", this.app.cameras);
+        //only get the cam1 and cam2
+        console.log("cameraNames", cameraNames);
+
+        cameraFolder.add(this.app, 'activeCameraName', [ 'Perspective', 'Left', 'Top', 'Bottom', "Right"] ).name("active camera");
+        /*
+        for(let m in this.app.cameras) {
+            console.log("m: "+ JSON.stringify(m))
+            cameraNames.push(m)
+        }
+        */
+
         cameraFolder.open()
     }
 }
--- tp2/engine/MyEngine.js
+++ tp2/engine/MyEngine.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { MySceneData } from './../parser/MySceneData.js';
 import { LightsEngine } from './LightsEngine.js';
 import { ComponentsEngine } from './ComponentsEngine.js';
-import { CamerasEngine } from './CamerasEngine.js';
 
 /**
  *  This class contains the contents of our engine
@@ -112,19 +111,66 @@ class MyEngine  {
 
 
     dealWithCameras() {
-        const cameras = this.data.getCameras();
-        if (cameras == null) return;
+        const aspect = window.innerWidth / window.innerHeight;
 
-        // create switch case for each camera type
-        for (let i = 0; i < cameras.length; i++) {
-            const cameraEngine = new CamerasEngine(cameras[i]);
-            const camera = cameraEngine.buildCamera();
-            this.app.cameras.push(camera);
+        const cameras = this.data.cameras;
+        if (cameras == null) return;
+        console.log ("my cameras", cameras);
+        // create cameras
+        for(let cam in cameras) {
+            console.log("cam: "+ JSON.stringify(cameras[cam]));
+            //if cam is perspective
+            if(cameras[cam].type == "perspective") {
+                const camera = new THREE.PerspectiveCamera(cameras[cam].fov, cameras[cam].an,cameras[cam].near, cameras[cam].far);
+                camera.position.set(cameras[cam].location[0], cameras[cam].location[1], cameras[cam].location[2]);
+                camera.lookAt(cameras[cam].target[0], cameras[cam].target[1], cameras[cam].target[2]);
+                this.app.cameras['Perspective'] = camera;
+            }
+            //if cam is orthogonal
+            if(cameras[cam].type == "orthogonal"){
+                this.dealWithOrthogonal(cameras,cam ,aspect);
+            }
+
+
         }
 
-        // set default camera
-        this.app.camera = this.app.cameras[0];
+
         console.info("Loaded Cameras");
+    }
+
+
+
+    dealWithOrthogonal(cameras,cam, aspect) {
+        const left = - cameras[cam].left / 2 * aspect
+        const right = cameras[cam].right /2 * aspect
+        const top = cameras[cam].top / 2
+        const bottom = -cameras[cam].bottom / 2
+        const near = -cameras[cam].near /2
+        const far =  cameras[cam].far
+        const cameraLeft = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+        const cameraRight = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+        const cameraTop = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+        const cameraBottom = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+
+        cameraLeft.up = new THREE.Vector3(0,1,0);
+        cameraLeft.position.set(-cameras[cam].location[0] /4,0,0);
+        cameraLeft.lookAt( cameras[cam].target);
+        this.app.cameras['Left'] = cameraLeft;
+
+        cameraRight.up = new THREE.Vector3(0,1,0);
+        cameraRight.position.set(cameras[cam].location[0] /4,0,0);
+        cameraRight.lookAt( cameras[cam].target );
+        this.app.cameras['Right'] = cameraRight;
+
+        cameraTop.up = new THREE.Vector3(0,0,1);
+        cameraTop.position.set(0, cameras[cam].location[1] /4, 0);
+        cameraTop.lookAt(cameras[cam].target);
+        this.app.cameras['Top'] = cameraTop;
+
+        cameraBottom.up = new THREE.Vector3(0,0,1);
+        cameraBottom.position.set(0, -cameras[cam].location[1] /4, 0);
+        cameraBottom.lookAt( cameras[cam].target );
+        this.app.cameras['Bottom'] = cameraBottom;
     }
 
     dealWithRoot() {
