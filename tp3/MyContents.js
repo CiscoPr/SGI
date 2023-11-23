57650405b837b5d65ae9c0de7a374174e581b2e9
Francisco Prada <up202004646@up.pt>
2023-11-23 00:44:59 +0000

    added cloud model

--- tp3/MyContents.js
+++ tp3/MyContents.js
@@ -50,9 +50,16 @@ class MyContents {
     //--------------------------------------------------------------------------------
 
     const loader = new GLTFLoader().setPath('models/');
+    loader.load('cloud.glb', async (gltf) => {
+      const model = gltf.scene;
+      model.scale.set(10.0, 10.0, 10.0);
+      this.app.scene.add(gltf.scene);
+    });
+
+    const loader2 = new GLTFLoader().setPath('models/');
     loader.load('tifa.glb', async (gltf) => {
       const model = gltf.scene;
-      model.position.set(0, 0, 0);
+      model.position.set(5, 0, 0);
       model.scale.set(10.0, 10.0, 10.0);
       this.app.scene.add(gltf.scene);
     });
