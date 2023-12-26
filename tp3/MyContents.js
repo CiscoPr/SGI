5df2dfac03c04da2abaea09fb739d3050f06c378
Francisco Prada <up202004646@up.pt>
2023-12-26 23:36:22 +0000

    added building

--- tp3/MyContents.js
+++ tp3/MyContents.js
@@ -128,18 +128,40 @@ class MyContents {
       model.traverse(function (object){
         if (object.isMesh) object.castShadow = true;
       });
+
+
       model.rotation.y = -Math.PI/2;
-      model.position.set(6000, 150, -4500);
-      model.scale.set(600.0, 600.0, 600.0);
+      model.position.set(6000, 350, -4500);
+      model.scale.set(1500.0, 1500.0, 1500.0);
       this.app.scene.add(model);
       const model2 = gltf.scene.clone();
       model2.traverse(function (object){
         if (object.isMesh) object.castShadow = true;
       });
       model2.rotation.y = -Math.PI/2;
-      model2.position.set(-6000, 150, -4500);
-      model2.scale.set(600.0, 600.0, 600.0);
+      model2.position.set(-6000, 350, -4500);
+      model2.scale.set(1500.0, 1500.0, 1500.0);
       this.app.scene.add(model2);
+
+    });
+
+    loader.load('buildings2.glb', async (gltf) => {
+
+      const model = gltf.scene;
+      model.traverse(function (object){
+        if (object.isMesh) object.castShadow = true;
+      });
+      model.position.set(-14000, 1360, -25000);
+      model.scale.set(-70000.0, 70000.0, 70000.0);
+      this.app.scene.add(model);
+      const model2 = gltf.scene.clone();
+      model2.traverse(function (object){
+        if (object.isMesh) object.castShadow = true;
+      });
+      model2.position.set(14000, 1390, 25500);
+      model2.scale.set(71000.0, 71000.0, -71000.0);
+      this.app.scene.add(model2);
+
     });
 
   }
