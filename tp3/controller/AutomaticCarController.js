d0a55bcc2d7a056d2b502f1cbbde6bc072bc1713
Francisco Prada <up202004646@up.pt>
2023-12-29 10:34:41 +0000

    minor changes

--- tp3/controller/AutomaticCarController.js
+++ tp3/controller/AutomaticCarController.js
@@ -23,7 +23,7 @@ class AutomaticCarController {
         this.mixerTime = 0;
         this.mixerPause = false;
         this.enableAnimationPosition = true;
-        this.animationMaxDuration = 60;
+        this.animationMaxDuration = 100;
         this.counter = 0;
 
         this.keyPoints = [
@@ -94,7 +94,7 @@ class AutomaticCarController {
 
     init(){
 
-        //this.debugKeyFrames();
+        this.debugKeyFrames();
         const positionKF = new THREE.VectorKeyframeTrack('.position', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
             24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57],
             [
@@ -370,7 +370,7 @@ class AutomaticCarController {
         this.mixer.update(delta/1.5)
         this.counter += 1;
         const nextCarAngle = this.model.rotation.y;
-        if(this.counter >= 15){
+        if(this.counter >= 20){
             this.wheelController = new WheelController(delta, this.model, this.wheels, currentCarAngle, nextCarAngle);
             this.wheelController.update();
         }
