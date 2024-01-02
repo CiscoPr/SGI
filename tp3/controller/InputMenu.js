d9ba0c6bbdad1d787d1df1c6f87f855e10b0de2c
Francisco Prada <up202004646@up.pt>
2024-01-02 18:31:37 +0000

    removed unecessary sprites

--- tp3/controller/InputMenu.js
+++ tp3/controller/InputMenu.js
@@ -187,7 +187,7 @@ class InputMenu {
             let j = Math.floor(pos / 16);
             let k = pos % 16;
             let letter = new THREE.PlaneGeometry( 1, 1 );
-            let texture = new THREE.TextureLoader().load( 'sprites/spriteLetters.png' );
+            let texture = new THREE.TextureLoader().load( './scene/textures/elements/spriteLetters.png' );
             texture.flipY = false;
             let offset = 1 / 16;
             let uv = [];
@@ -248,7 +248,7 @@ class InputMenu {
             let j = Math.floor(pos / 16);
             let k = pos % 16;
             let letter = new THREE.PlaneGeometry( 1, 1 );
-            let texture = new THREE.TextureLoader().load( 'sprites/spriteLetters.png' );
+            let texture = new THREE.TextureLoader().load( './scene/textures/elements/spriteLetters.png' );
             texture.flipY = false;
             let offset = 1 / 16;
             let uv = [];
